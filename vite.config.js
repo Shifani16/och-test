4c87f1a7fb3613bd544ff4659ccea5a6f246d0a1
Shifani16 <110917558+Shifani16@users.noreply.github.com>
2025-04-22 12:38:39 +0700

    pleek help the assets wont show up

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    base: '/build/',
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue(),
-        tailwindcss(),
-    ],
+export default defineConfig(({ command, mode }) => {
+    const isProduction = mode === 'production';
+    return {
+        base: isProduction ? 'https://och-test-production.up.railway.app/build/' : '/build/',
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            vue(),
+            tailwindcss(),
+        ],
+    };
 });
