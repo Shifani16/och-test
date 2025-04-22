170f14ce780f16f5da10a947584ff00f0993b74f
Shifani16 <110917558+Shifani16@users.noreply.github.com>
2025-04-22 12:44:58 +0700

    help assets pls

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,16 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ command, mode }) => {
-    const isProduction = mode === 'production';
-    return {
-        base: isProduction ? 'https://och-test-production.up.railway.app/build/' : '/build/',
-        plugins: [
-            laravel({
-                input: ['resources/css/app.css', 'resources/js/app.js'],
-                refresh: true,
-            }),
-            vue(),
-            tailwindcss(),
-        ],
-    };
-});
+export default defineConfig(({ mode }) => ({
+    base: mode === 'production' 
+        ? 'https://och-test-production.up.railway.app/build/' 
+        : '/build/',
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        vue(),
+        tailwindcss(),
+    ],
+}));
